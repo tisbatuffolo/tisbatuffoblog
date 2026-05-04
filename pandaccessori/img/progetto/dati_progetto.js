70e615466b00baace8fab19a46163c6a15d08a1b
tisbatuffolo <dommer87@gmail.com>
2026-05-04 23:42:05 +0200

    restore dati_progetto.js senza Ioooo.jpg

--- pandaccessori/img/progetto/dati_progetto.js
+++ pandaccessori/img/progetto/dati_progetto.js
@@ -3,9 +3,5 @@ const dati_progetto = [
     {
         "titolo": "PandaTazza",
         "immagine": "PandaTazza.jpg"
-    },
-    {
-        "titolo": "Ioooo",
-        "immagine": "Ioooo.jpg"
     }
 ];
