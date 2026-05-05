3c9264cabf3ee64ac12cbbfe312aad86cbee84e6
tisbatuffolo <dommer87@gmail.com>
2026-05-05 21:18:42 +0200

    Clean up project data by removing unused entries
    Removed unused project data entries from the array.

--- pandaccessori/img/progetto/dati_progetto.js
+++ pandaccessori/img/progetto/dati_progetto.js
@@ -3,13 +3,5 @@ const dati_progetto = [
     {
         "titolo": "PandaTazza",
         "immagine": "PandaTazza.jpg"
-    },
-    {
-        "titolo": "test",
-        "immagine": "test.jpg"
-    },
-    {
-        "titolo": "Cucu",
-        "immagine": "Cucu.jpg"
     }
 ];
