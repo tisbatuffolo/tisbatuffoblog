d56c704621008cdcb4c15271446cf149eff4abd2
tisbatuffolo <dommer87@gmail.com>
2026-05-05 21:59:22 +0200

    Aggiornato DB Pandaccessori via API Edit

--- pandaccessori/img/progetto/dati_progetto.js
+++ pandaccessori/img/progetto/dati_progetto.js
@@ -3,5 +3,9 @@ const dati_progetto = [
     {
         "titolo": "PandaTazza",
         "immagine": "PandaTazza.jpg"
+    },
+    {
+        "titolo": "ciaooo in progetto",
+        "immagine": "ciaooo in progetto.jpg"
     }
 ];
